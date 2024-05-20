15a0f1b16c1e160831939e07e8560bed88cfcf96
NoailletasJordan <j.noailletas@gmail.com>
2024-05-20 21:46:24 +0700

    adding clamp to card's texts

--- components/Article/components/SideArticle/index.js
+++ components/Article/components/SideArticle/index.js
@@ -3,10 +3,7 @@ import Link from 'next/link'
 import React from 'react'
 import styles from './side-article.module.scss'
 
-export default function ArticleMini({ article, noTruncate, url }) {
-  const truncateNumber = noTruncate
-    ? { title: undefined, desc: undefined }
-    : { title: 180, desc: 80 }
+export default function ArticleMini({ article, url }) {
   return (
     <Link href={url || ''} key={article.fields.title}>
       <div className={styles.aside__article}>
@@ -19,16 +16,12 @@ export default function ArticleMini({ article, noTruncate, url }) {
           />
         </div>
         <div className={styles.aside__article__title}>
-          {truncate(article.fields.title, truncateNumber.title)}
+          {article.fields.title}
         </div>
         <div className={styles.aside__article__description}>
-          {truncate(article.fields.description, truncateNumber.desc)}
+          {article.fields.description}
         </div>
       </div>
     </Link>
   )
-}
-
-function truncate(source, size) {
-  return source.length > size ? source.slice(0, size - 1) + '…' : source
 }
